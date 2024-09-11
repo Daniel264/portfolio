fe267daf60fea25499ec7b0a00c57c6e80028c29
Daniel Olatinsu <danieloluwafolajimi@gmail.com>
2024-09-11 20:48:20 +0100

    Add tooltip to each icon

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -5,17 +5,17 @@ const Menu = () => {
         <div>
             <ul className="menu hidden rounded-box bg-inherit sm:flex">
                 <li>
-                    <a href="https://github.com/Daniel264" className="tooltip tooltip-right" data-tip="GitHub" target="_blank" rel="noopener noreferrer">
+                    <a href="https://github.com/Daniel264" className="tooltip tooltip-right" data-tip="Twitter" target="_blank" rel="noopener noreferrer">
                         <i className="fab fa-twitter fa-2x"></i> {/* Use fa-2x to scale the icon */}
                     </a>
                 </li>
                 <li>
-                    <a href="https://github.com/Daniel264" className="tooltip tooltip-right" data-tip="GitHub" target="_blank" rel="noopener noreferrer">
+                    <a href="https://github.com/Daniel264" className="tooltip tooltip-right" data-tip="Instagram" target="_blank" rel="noopener noreferrer">
                         <i className="fab fa-instagram fa-2x"></i> {/* Use fa-2x to scale the icon */}
                     </a>
                 </li>
                 <li>
-                    <a href="https://github.com/Daniel264" className="tooltip tooltip-right" data-tip="GitHub" target="_blank" rel="noopener noreferrer">
+                    <a href="https://github.com/Daniel264" className="tooltip tooltip-right" data-tip="facebook" target="_blank" rel="noopener noreferrer">
                         <i className="fab fa-facebook fa-2x"></i> {/* Use fa-2x to scale the icon */}
                     </a>
                 </li>
