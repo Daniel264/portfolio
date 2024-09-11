d973359cbc522e0012ebea44cefca41bdeef8ae4
Daniel Olatinsu <danieloluwafolajimi@gmail.com>
2024-09-11 19:02:58 +0100

    adjust image for sm devices

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -13,11 +13,18 @@ export default function Home() {
             <main className="flex min-h-screen flex-col items-center bg-[#31363F] p-5 ">
                 {/* <div className="m-auto w-full max-w-8xl"> */}
 
-                <div className="max-w-8xl md:m-auto text-white">
-                    <div className="hero md:min-h-screen bg-inherit">
+                <div className="max-w-8xl text-white md:m-auto">
+                    <div className="hero bg-inherit md:min-h-screen">
                         <div className="hero-content flex-col-reverse text-center lg:flex-row-reverse">
-                            <div className="w-full">
-                                <Image src="/assets/images/me.jpg" alt="my-profile-pic" width={450} height={40} className="h-[22rem] rounded-full shadow-2xl md:h-[29rem]" />
+                            <div className="flex w-full justify-center">
+                                <Image
+                                    src="/assets/images/me.jpg"
+                                    alt="my-profile-pic"
+                                    width={450}
+                                    height={40}
+                                    className="h-[22rem] rounded-full shadow-2xl
+                                sm:h-[29rem]"
+                                />
                             </div>
                             <div className="w-full">
                                 <h3 className="my-5 text-5xl font-semibold">Hello, I&apos;m Daniel Olatinsu</h3>
