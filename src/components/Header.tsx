577257ffa08e94ea5137b40beb363b39b90ab5ff
Daniel Olatinsu <danieloluwafolajimi@gmail.com>
2024-09-11 15:13:11 +0100

    Add navbar button to header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -28,9 +28,11 @@ const Header = () => {
                         </div>
                     </div>
                     <div className="dropdown dropdown-end">
-                        <div tabIndex={0} role="button" className="avatar btn btn-circle btn-ghost">
+                        <div tabIndex={0} role="button" className=" btn btn-ghost">
                             <div className="w-10 rounded-full">
-                                <img alt="Tailwind CSS Navbar component" src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp" />
+                                <svg className="h-5 w-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
+                                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 1h15M1 7h15M1 13h15" />
+                                </svg>
                             </div>
                         </div>
                         <ul tabIndex={0} className="menu dropdown-content menu-sm z-[1] mt-3 w-52 rounded-box bg-base-100 p-2 shadow">
