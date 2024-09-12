33cbb47b8824e48d38d11183bfb34a77dc32f293
Daniel Olatinsu <danieloluwafolajimi@gmail.com>
2024-09-12 12:37:42 +0100

    Refactor TopNavigation component to use Link and button elements for navigation

--- src/components/TopNavigation.tsx
+++ src/components/TopNavigation.tsx
@@ -54,14 +54,18 @@ const TopNavigation = () => {
                                 </Link>
                             </li>
                             <li>
-                                <a href="#" className="block rounded px-3 py-2 text-gray-900 hover:bg-gray-100 md:border-0 md:p-0 md:hover:bg-transparent md:hover:text-blue-700 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent md:dark:hover:text-blue-500">
-                                    Portfolio
-                                </a>
+                                <Link href="/portfolio">
+                                    <button  className="block rounded px-3 py-2 text-gray-900 hover:bg-gray-100 md:border-0 md:p-0 md:hover:bg-transparent md:hover:text-blue-700 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent md:dark:hover:text-blue-500">
+                                        Portfolio
+                                    </button>
+                                </Link>
                             </li>
                             <li>
-                                <a href="#" className="block rounded px-3 py-2 text-gray-900 hover:bg-gray-100 md:border-0 md:p-0 md:hover:bg-transparent md:hover:text-blue-700 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent md:dark:hover:text-blue-500">
-                                    Contact
-                                </a>
+                                <Link href="/contact">
+                                    <button className="block rounded px-3 py-2 text-gray-900 hover:bg-gray-100 md:border-0 md:p-0 md:hover:bg-transparent md:hover:text-blue-700 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent md:dark:hover:text-blue-500">
+                                        Contact
+                                    </button>
+                                </Link>
                             </li>
                         </ul>
                     </div>
