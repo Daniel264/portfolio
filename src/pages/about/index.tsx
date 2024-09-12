bae2411ad31b2292ef581e0bc25b3fb4a4fc156e
Daniel Olatinsu <danieloluwafolajimi@gmail.com>
2024-09-12 02:17:55 +0100

    Partially Style the about page

--- src/pages/about/index.tsx
+++ src/pages/about/index.tsx
@@ -7,11 +7,19 @@ const About = () => {
             <main className="flex h-[100vh] w-full items-center justify-center bg-[#31363F] px-[2rem] text-center text-base-200 lg:px-[5rem]">
                 <Menu />
                 <div>
-                    <h1 className="font text-5xl font-semibold">About Me</h1>
-                    <div>
-                        <p>Fullstack Developer & Mobile Enthusiast</p>
-                        <p>Currently residing in Lagos, Nigeria</p>
-                        <p>Hi!!! I am a Full-stack developer with a passion for mobile technology and a love for continuous learning. Adept at creating innovative solutions and excited by emerging tech trends</p>
+                    <h1 className="font text-5xl font-semibold">
+                        About <span className="font-medium text-slate-400">Daniel</span>
+                    </h1>
+                    <div className="py-8">
+                        <p className="pb-2 text-xl">
+                            <i className="fa-solid fa-code mr-3"></i>Fullstack Developer & Mobile Enthusiast
+                        </p>
+                        <p>
+                            <i className="fa-solid fa-location-dot mr-4"></i>Currently residing in Lagos, Nigeria
+                        </p>
+                        <p>
+                            Hi!!! I am a Full-stack developer with a passion for mobile technology and a <br /> love for continuous learning. Adept at creating innovative solutions and <br /> excited by emerging tech trends
+                        </p>
                     </div>
                 </div>
             </main>
