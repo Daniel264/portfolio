78f273580a497051f1fa0f04f11c3845e6e2476a
Daniel Olatinsu <danieloluwafolajimi@gmail.com>
2024-09-12 13:06:58 +0100

    change bg-color and text color of contact page

--- src/pages/contact/index.tsx
+++ src/pages/contact/index.tsx
@@ -10,13 +10,13 @@ const Contact = () => {
         <>
             {loading && <div className="loading-animation"></div>}
             <div className="contact-page h-[100vh] bg-[#31363F]">
-                <div className="hero min-h-screen bg-base-200">
+                <div className="hero min-h-screen text-white">
                     <div className="hero-content flex-col lg:flex-row-reverse">
                         <div className="text-center lg:text-left">
                             <h1 className="text-5xl font-bold">Login now!</h1>
                             <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
                         </div>
-                        <div className="card w-full max-w-sm shrink-0 bg-base-100 shadow-2xl">
+                        <div className="card w-full max-w-sm shrink-0 bg-slate-300 shadow-2xl">
                             <form className="card-body">
                                 <div className="form-control">
                                     <label className="label">
