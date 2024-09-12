8a3f1c33767513935c7f8f5db702df78ca4d61f4
Daniel Olatinsu <danieloluwafolajimi@gmail.com>
2024-09-12 13:22:25 +0100

    Refactor Contact page to adjust text alignment, width, and form layout

--- src/pages/contact/index.tsx
+++ src/pages/contact/index.tsx
@@ -12,28 +12,33 @@ const Contact = () => {
             <div className="contact-page h-[100vh] bg-[#31363F]">
                 <div className="hero min-h-screen text-white">
                     <div className="hero-content flex-col lg:flex-row">
-                        <div className="text-center lg:text-left lg:w-1/2">
+                        <div className="text-center lg:text-left">
                             <h1 className="text-5xl font-bold">Login now!</h1>
                             <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
                         </div>
-                        <div className="card w-full max-w-sm shrink-0 bg-slate-300 shadow-2xl">
+                        <div className="card max-h-96 w-full max-w-sm shrink-0 shadow-2xl lg:max-w-2xl">
                             <form className="card-body">
-                                <div className="form-control">
-                                    <label className="label">
-                                        <span className="label-text">Email</span>
-                                    </label>
-                                    <input type="email" placeholder="email" className="input input-bordered" required />
+                                <div className="flex space-x-5">
+                                    <div className="form-control">
+                                        <label className="label">
+                                            <span className="label-text text-white">Full Name</span>
+                                        </label>
+                                        <input type="email" placeholder="email" className="input input-bordered" required />
+                                    </div>
+                                    <div className="form-control">
+                                        <label className="label">
+                                            <span className="label-text text-white">Email</span>
+                                        </label>
+                                        <input type="password" placeholder="password" className="input input-bordered" required />
+                                    </div>
                                 </div>
-                                <div className="form-control">
-                                    <label className="label">
-                                        <span className="label-text">Password</span>
-                                    </label>
-                                    <input type="password" placeholder="password" className="input input-bordered" required />
-                                    <label className="label">
-                                        <a href="#" className="link-hover link label-text-alt">
-                                            Forgot password?
-                                        </a>
-                                    </label>
+                                <div>
+                                    <div className="form-control">
+                                        <label className="label">
+                                            <span className="label-text text-white">Full Name</span>
+                                        </label>
+                                        <input type="email" placeholder="email" className="input input-bordered" required />
+                                    </div>
                                 </div>
                                 <div className="form-control mt-6">
                                     <button className="btn btn-primary">Login</button>
