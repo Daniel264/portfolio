cc287db0dc969661f6f0d50f82a6fb9c906e2791
Daniel Olatinsu <danieloluwafolajimi@gmail.com>
2024-09-12 02:03:50 +0100

    align items to the center

--- src/pages/about/index.tsx
+++ src/pages/about/index.tsx
@@ -2,9 +2,18 @@ import React from "react";
 
 const About = () => {
     return (
-        <div>
-            <h1>About</h1>
-        </div>
+        <>
+            <main className="h-[100vh] w-full bg-[#31363F] text-center text-base-200 flex justify-center items-center">
+                <div>
+                    <h1 className="font text-5xl font-semibold">About Me</h1>
+                    <div>
+                        <p>Fullstack Developer & Mobile Enthusiast</p>
+                        <p>Currently residing in Lagos, Nigeria</p>
+                        <p>Hi!!! I am a Full-stack developer with a passion for mobile technology and a love for continuous learning. Adept at creating innovative solutions and excited by emerging tech trends</p>
+                    </div>
+                </div>
+            </main>
+        </>
     );
 };
 
