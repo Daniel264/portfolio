aefe01d1de78b8565deff27a39f6cd66c79d91e0
Daniel Olatinsu <danieloluwafolajimi@gmail.com>
2024-09-12 13:10:07 +0100

    Refactor Contact page to adjust text alignment and width of the hero section

--- src/pages/contact/index.tsx
+++ src/pages/contact/index.tsx
@@ -12,7 +12,7 @@ const Contact = () => {
             <div className="contact-page h-[100vh] bg-[#31363F]">
                 <div className="hero min-h-screen text-white">
                     <div className="hero-content flex-col lg:flex-row-reverse">
-                        <div className="text-center lg:text-left">
+                        <div className="text-center lg:text-left lg:w-1/2">
                             <h1 className="text-5xl font-bold">Login now!</h1>
                             <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
                         </div>
