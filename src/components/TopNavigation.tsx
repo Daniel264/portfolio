9673725c9af452b0816574529e3fbe3c56746283
Daniel Olatinsu <danieloluwafolajimi@gmail.com>
2024-09-12 01:42:55 +0100

    Uncapitalize navbar texts

--- src/components/TopNavigation.tsx
+++ src/components/TopNavigation.tsx
@@ -37,7 +37,7 @@ const TopNavigation = () => {
                         <ul className="mt-4 flex flex-col rounded-lg border border-gray-100 bg-gray-50 p-4 font-medium md:mt-0 md:flex-row md:space-x-8 md:border-0 md:bg-white md:p-0 rtl:space-x-reverse dark:border-none dark:bg-inherit ">
                             <li>
                                 <a href="#" className="block rounded px-3 py-2 text-white md:bg-transparent md:p-0 " aria-current="page">
-                                    HOME
+                                    Home
                                 </a>
                             </li>
                             <li>
@@ -47,18 +47,18 @@ const TopNavigation = () => {
                                         data-dropdown-toggle="dropdownNavbar"
                                         className="flex w-full items-center justify-between px-3 py-2 text-gray-900 hover:bg-gray-100 md:w-auto md:border-0 md:p-0 md:hover:bg-transparent md:hover:text-blue-700 dark:text-white dark:hover:bg-gray-700 dark:focus:text-white md:dark:hover:bg-transparent md:dark:hover:text-blue-500"
                                     >
-                                        ABOUT
+                                        About
                                     </button>
                                 </Link>
                             </li>
                             <li>
                                 <a href="#" className="block rounded px-3 py-2 text-gray-900 hover:bg-gray-100 md:border-0 md:p-0 md:hover:bg-transparent md:hover:text-blue-700 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent md:dark:hover:text-blue-500">
-                                    PORTFOLIO
+                                    Portfolio
                                 </a>
                             </li>
                             <li>
                                 <a href="#" className="block rounded px-3 py-2 text-gray-900 hover:bg-gray-100 md:border-0 md:p-0 md:hover:bg-transparent md:hover:text-blue-700 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent md:dark:hover:text-blue-500">
-                                    CONTACT
+                                    Contact
                                 </a>
                             </li>
                         </ul>
