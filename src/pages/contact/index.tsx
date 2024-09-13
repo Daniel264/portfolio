034fe23f1a2f80b6c0709f2e5bf826e1c7a75f6a
Daniel Olatinsu <danieloluwafolajimi@gmail.com>
2024-09-13 01:38:22 +0100

    Add label to all the input fields

--- src/pages/contact/index.tsx
+++ src/pages/contact/index.tsx
@@ -13,61 +13,60 @@ const Contact = () => {
             {loading && <div className="loading-animation"></div>}
             <TopNavigation />
             <div className="contact-page bg-[#101010] px-2 py-20 text-center md:px-10 lg:py-0 ">
-                <div className="hero flex min-h-screen flex-col items-center justify-center text-white pt-20 md:pt-24 lg:pt-32">
-                    <h1 className="mb-10 text-5xl font-bold">
-                        Contact <span className="text-[#5D5CD6]">Me</span>
-                    </h1>
-                    <p className="pb-7 text-3xl font-light">
-                        <span className="text-primary">get</span> in touch ...
-                    </p>
-                    <div className="flex h-fit w-full flex-col items-center justify-center gap-4 md:gap-7 lg:flex-row">
-                        <div className="flex flex-col h-[160px] w-[80%] items-center justify-center rounded-3xl bg-black shadow-xl lg:w-[50%] ">
-                            <i className="fa-regular fa-envelope fa-2x"></i>
-                        
-                            <p className="text-xl text-[#D1D5DB] md:text-2xl">danielolatinsu@gmail.com</p>
-                        </div>
-                        <div className="flex flex-col h-[160px] w-[80%] items-center justify-center rounded-3xl bg-black shadow-xl lg:w-[50%]">
-                            <i className="fa-solid fa-location-dot fa-2x "></i>
-                            <p className="text-xl text-[#D1D5DB] md:text-2xl">Lagos, lagos Nigeria</p>
-                        </div>
-                    </div>
-                    <div className="hero-content w-full flex-col lg:flex-row">
-                        <div className="text-center lg:text-left">
-                            <div className="py-6">
-                                <h2 className="text-2xl font-medium">Contact Info</h2>
-                            </div>
-                        </div>
-                        <div className="card max-h-fit w-full shrink-0 bg-[#101010] shadow-2xl md:max-w-2xl">
-                            <form className="card-body">
-                                <div className="space-y-2 lg:flex lg:space-x-5 lg:space-y-0">
-                                    <div className="form-control w-full">
-                                        <input
-                                            type="text"
-                                            placeholder="name..."
-                                            className="input input-bordered border-none 
-                                        bg-black placeholder:text-[#D1D5DB]"
-                                            required
-                                        />
-                                    </div>
-                                    <div className="form-control w-full">
-                                        <input type="email" placeholder="email..." className="input input-bordered border-none bg-black placeholder:text-[#D1D5DB]" required />
-                                    </div>
-                                </div>
-                                <div>
-                                    <div className="form-control">
-                                        <input type="text" placeholder="Title..." className="input input-bordered border-none bg-black placeholder:text-[#D1D5DB]" required />
-                                    </div>
-                                </div>
-                                <div>
-                                    <textarea placeholder="Enter Message ..." className="textarea textarea-bordered textarea-md w-full max-w-full border-none bg-black placeholder:text-[#D1D5DB]"></textarea>
-                                </div>
-                                <div className="form-control mt-6">
-                                    <button className="btn border-none bg-primary text-[#D1D5DB]">Submit</button>
-                                </div>
-                            </form>
-                        </div>
-                    </div>
+            <div className="hero flex min-h-screen flex-col items-center justify-center text-white pt-20 md:pt-24 lg:pt-32">
+                <h1 className="mb-10 text-5xl font-bold">
+                Contact <span className="text-[#5D5CD6]">Me</span>
+                </h1>
+                <p className="pb-7 text-3xl font-light">
+                <span className="text-primary">get</span> in touch ...
+                </p>
+                <div className="flex h-fit w-full flex-col items-center justify-center gap-4 md:gap-7 lg:flex-row">
+                <div className="flex flex-col h-[160px] w-[80%] items-center justify-center rounded-3xl bg-black shadow-xl lg:w-[50%] ">
+                    <i className="fa-regular fa-envelope fa-2x"></i>
+                
+                    <p className="text-xl text-[#D1D5DB] md:text-2xl">danielolatinsu@gmail.com</p>
                 </div>
+                <div className="flex flex-col h-[160px] w-[80%] items-center justify-center rounded-3xl bg-black shadow-xl lg:w-[50%]">
+                    <i className="fa-solid fa-location-dot fa-2x "></i>
+                    <p className="text-xl text-[#D1D5DB] md:text-2xl">Lagos, lagos Nigeria</p>
+                </div>
+                </div>
+                <div className="hero-content w-full flex-col lg:flex-row">
+                <div className="card max-h-fit w-full shrink-0 bg-[#101010] shadow-2xl md:max-w-2xl">
+                    <form className="card-body">
+                    <div className="space-y-2 lg:flex lg:space-x-5 lg:space-y-0">
+                        <div className="form-control w-full">
+                        <label className="label text-[#D1D5DB]">Name</label>
+                        <input
+                            type="text"
+                            placeholder="name..."
+                            className="input input-bordered border-none 
+                        bg-black placeholder:text-[#D1D5DB]"
+                            required
+                        />
+                        </div>
+                        <div className="form-control w-full">
+                        <label className="label text-[#D1D5DB]">Email</label>
+                        <input type="email" placeholder="email..." className="input input-bordered border-none bg-black placeholder:text-[#D1D5DB]" required />
+                        </div>
+                    </div>
+                    <div>
+                        <div className="form-control">
+                        <label className="label text-[#D1D5DB]">Title</label>
+                        <input type="text" placeholder="Title..." className="input input-bordered border-none bg-black placeholder:text-[#D1D5DB]" required />
+                        </div>
+                    </div>
+                    <div>
+                        <label className="label text-[#D1D5DB]">Message</label>
+                        <textarea placeholder="Enter Message ..." className="textarea textarea-bordered textarea-md w-full max-w-full border-none bg-black placeholder:text-[#D1D5DB]"></textarea>
+                    </div>
+                    <div className="form-control mt-6">
+                        <button className="btn border-none bg-primary text-[#D1D5DB]">Submit</button>
+                    </div>
+                    </form>
+                </div>
+                </div>
+            </div>
             </div>
         </>
     );
