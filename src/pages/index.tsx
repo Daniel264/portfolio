09589d948f81aa4b9ba3fcc3dec4b6194e674938
Daniel Olatinsu <danieloluwafolajimi@gmail.com>
2024-09-13 01:25:21 +0100

    move menu component to the left side of the page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -20,23 +20,16 @@ export default function Home() {
             <TopNavigation />
 
             <main className="flex min-h-screen flex-col items-center bg-[#101010] p-5 pt-28">
-                {/* <div className="m-auto w-full max-w-8xl"> */}
-
-                <div className="max-w-8xl flex text-white md:m-auto">
-                    <Menu />
-                    <div className="flex h-full w-full flex-col  items-center">
+                <div className="max-w-8xl flex w-full text-white md:m-auto">
+                    <div className="fixed left-0 top-44 hidden h-full md:flex">
+                        <Menu />
+                    </div>
+                    <div className="ml-0 flex h-full w-full flex-col items-center md:ml-20">
                         <div className="flex flex-col sm:flex-row">
                             <div className="hero bg-inherit">
-                                <div className="hero-content flex-col-reverse text-center lg:flex-row-reverse">
+                                <div className="hero-content flex-col text-center lg:flex-row-reverse">
                                     <div className="flex w-full justify-center">
-                                        <Image
-                                            src="/assets/images/me.jpg"
-                                            alt="my-profile-pic"
-                                            width={450}
-                                            height={40}
-                                            className="h-[22rem] rounded-full shadow-2xl
-                                        sm:h-[29rem]"
-                                        />
+                                        <Image src="/assets/images/me.jpg" alt="my-profile-pic" width={450} height={40} className="h-[22rem] rounded-full shadow-2xl sm:h-[29rem]" />
                                     </div>
                                     <div className="w-full">
                                         <h3 className="my-5 text-5xl font-semibold">
@@ -50,11 +43,11 @@ export default function Home() {
                             </div>
                             {/* <Stats /> */}
                         </div>
-                        <div className="ml-5 mt-8 space-y-4 md:ml-0 md:mr-44 md:space-x-5">
-                            <button className="btn w-[22rem] rounded border-none bg-primary text-white shadow-2xl hover:animate-bounce hover:bg-slate-500 md:w-44">
+                        <div className="ml-0 mt-8 space-y-4 md:ml-0 md:mr-44 md:space-x-5">
+                            <button className="btn w-full rounded border-none bg-primary text-white shadow-2xl hover:animate-bounce hover:bg-slate-500 md:w-44">
                                 Lets Talk <i className="fa-solid fa-message"></i>
                             </button>
-                            <button onClick={handleLoading} className="btn w-[22rem] rounded border-none bg-primary text-white shadow-2xl hover:animate-bounce hover:bg-slate-500 md:w-44">
+                            <button onClick={handleLoading} className="btn w-full rounded border-none bg-primary text-white shadow-2xl hover:animate-bounce hover:bg-slate-500 md:w-44">
                                 Download CV <i className="fa-solid fa-download"></i>
                             </button>
                         </div>
