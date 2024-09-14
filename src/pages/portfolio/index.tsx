a4e2e40da781cf272ac199fb7b166a13d8ff9d68
Daniel Olatinsu <danieloluwafolajimi@gmail.com>
2024-09-14 21:16:35 +0100

    adjust responsiveness of projects section

--- src/pages/portfolio/index.tsx
+++ src/pages/portfolio/index.tsx
@@ -25,41 +25,6 @@ const Portfolio: React.FC = () => {
             width: 1080,
             height: 720,
         },
-        // {
-        //     title: "ChatGPT dry-jokes",
-        //     description: "description",
-        //     imageUrl: "/assets/images/jokes.png",
-        //     width: 1080,
-        //     height: 720,
-        // },
-        // {
-        //     title: "Foodland",
-        //     description: "description",
-        //     imageUrl: "/assets/images/foodland.png",
-        //     width: 1080,
-        //     height: 720,
-        // },
-        // {
-        //     title: "My Blog app",
-        //     description: "description",
-        //     imageUrl: "/assets/images/blog.png",
-        //     width: 1080,
-        //     height: 720,
-        // },
-        // {
-        //     title: "Todo List.",
-        //     description: "description",
-        //     imageUrl: "/assets/images/todo.png",
-        //     width: 1080,
-        //     height: 720,
-        // },
-        // {
-        //     title: "Password Generator.",
-        //     description: "Password.",
-        //     imageUrl: "/assets/images/password.png",
-        //     width: 1080,
-        //     height: 720,
-        // },
         // Add more projects as needed
     ];
 
@@ -67,31 +32,34 @@ const Portfolio: React.FC = () => {
         <div id="portfolio" className="">
             <TopNavigation />
             <div className="flex min-h-screen w-full flex-col items-center">
-                {works.map((work, index) => (
-                    <div key={index} className="card bg-base-100 w-96 shadow-xl my-4">
-                        <figure>
-                            <Image
-                                src={work.imageUrl}
-                                width={work.width}
-                                height={work.height}
-                                alt={work.title}
-                                className="rounded"
-                                quality={100} // Set image quality to maximum
-                                priority={index < 2} // Prioritize the first two images
-                                sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw" // Responsive sizes
-                            />
-                        </figure>
-                        <div className="card-body">
-                            <h2 className="card-title">{work.title}</h2>
-                            <p>{work.description}</p>
-                            <div className="card-actions justify-end">
-                                <button className="btn btn-primary">View Project</button>
+                <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 w-full px-4">
+                    {works.map((work, index) => (
+                        <div key={index} className="card bg-base-100 w-full shadow-xl my-4">
+                            <figure>
+                                <Image
+                                    src={work.imageUrl}
+                                    width={work.width}
+                                    height={work.height}
+                                    alt={work.title}
+                                    className="rounded"
+                                    quality={100} // Set image quality to maximum
+                                    priority={index < 2} // Prioritize the first two images
+                                    sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw" // Responsive sizes
+                                />
+                            </figure>
+                            <div className="card-body">
+                                <h2 className="card-title">{work.title}</h2>
+                                <p>{work.description}</p>
+                                <div className="card-actions justify-end">
+                                    <button className="btn btn-primary">View Project</button>
+                                </div>
                             </div>
                         </div>
-                    </div>
-                ))}
+                    ))}
+                </div>
             </div>
         </div>
     );
 };
+
 export default Portfolio;
