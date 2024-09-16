b9a3911909a1d3e5860e529f2ab2f985326c1672
Daniel Olatinsu <danieloluwafolajimi@gmail.com>
2024-09-16 18:04:49 +0100

    add more frontend skills

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -3,24 +3,26 @@ import React from "react";
 const Skills = () => {
     return (
         <main className="flex h-full w-full flex-col items-center text-white">
-            <h1 className="text-center text-6xl font-bold text-white lg:text-7xl">Skills</h1>
-            <p>DEVELOPMENT ARSENAL</p>
+            <h1 className="pb-5 text-center text-6xl font-bold text-white lg:text-7xl">Skills</h1>
+            <p className="pb-5">DEVELOPMENT ARSENAL</p>
             <section className="grid grid-cols-1 gap-10 md:grid-cols-3 md:grid-rows-3">
                 <article className="col-start-1 row-start-1">
                     <h2 className="text-5xl font-semibold">Frontend</h2>
                     <ul className="text-lg">
                         <li>TypeScript</li>
-                        <li>JavaScript</li>
+                        <li>JavaScript(ES6+)</li>
                         <li>ViteTest</li>
                         <li>React</li>
                         <li>Next.js</li>
+                        <li>Git/GitHub</li>
+                        <li>NextAuth</li>
                         <div className="ml-44 flex w-max">
                             <p>95%</p>
                         </div>
                         <progress className="progress progress-primary w-56 bg-white" value="95" max="100"></progress>
                     </ul>
                 </article>
-                <article className="col-start-1 md:col-start-2 row-start-2">
+                <article className="col-start-1 row-start-2 md:col-start-2">
                     <h2 className="text-5xl font-semibold">Backend</h2>
                     <ul className="text-lg">
                         <li>Node.js</li>
@@ -35,7 +37,7 @@ const Skills = () => {
                         <progress className="progress progress-primary w-56 bg-white" value="80" max="100"></progress>
                     </ul>
                 </article>
-                <article className="col-start-1 md:col-start-3 row-start-3">
+                <article className="col-start-1 row-start-3 md:col-start-3">
                     <h2 className="text-5xl font-semibold">UI Libraries</h2>
                     <ul className="text-lg">
                         <li>GSAP</li>
