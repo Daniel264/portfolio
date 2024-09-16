787474009ecdfd37b80ca98bb1101c6071640a5f
Daniel Olatinsu <danieloluwafolajimi@gmail.com>
2024-09-16 14:55:30 +0100

    add icon for submit button in contact page

--- src/pages/contact/index.tsx
+++ src/pages/contact/index.tsx
@@ -22,11 +22,14 @@ const Contact = () => {
             {loading && <div className="loading-animation"></div>}
             <TopNavigation />
             <div id="contact" className="contact-page px-2 py-20 text-center md:px-10 lg:py-0">
-                <div className="hero flex min-h-screen flex-col items-center justify-center pt-20 text-white md:pt-24 lg:pt-32 lg:flex-row lg:items-start lg:justify-between">
+                <div className="hero flex min-h-screen flex-col items-center justify-center pt-20 text-white md:pt-24 lg:flex-row lg:items-start lg:justify-between lg:pt-32">
                     <div className="lg:w-1/2 lg:pr-10">
-                        <h1 id="#big" className="mb-10 text-5xl md:text-7xl font-bold">Get In Touch</h1>
+                        <h1 id="#big" className="mb-10 text-5xl font-bold md:text-7xl">
+                            Get In Touch
+                        </h1>
                         <p className="reveal-text text-center text-lg">
-                            Currently, i&apos;m looking for new opportunities. <br /> If you have one, my inbox is always open. <br /> Whether you have a job proposal or question <br />or you just want to say hi, I&apos;ll definitely get back to you.
+                            Currently, i&apos;m looking for new opportunities. <br /> If you have one, my inbox is always open. <br /> Whether you have a job proposal or question <br />
+                            or you just want to say hi, I&apos;ll definitely get back to you.
                         </p>
                     </div>
                     <div className="hero-content w-full flex-col lg:w-1/2 lg:flex-row">
@@ -44,11 +47,18 @@ const Contact = () => {
                                 </div>
                                 <div>
                                     <label className="label text-[#D1D5DB]">Message</label>
-                                    <textarea className="textarea textarea-md w-full max-w-full border-2 border-[#212531] bg-black  bg-transparent placeholder:text-[#D1D5DB] placeholder:text-base" placeholder="hi! your services will be needed at our company X. How quickly can you hop on this?"></textarea>
+                                    <textarea
+                                        className="textarea textarea-md w-full max-w-full border-2 border-[#212531] bg-black  bg-transparent placeholder:text-base placeholder:text-[#D1D5DB]"
+                                        placeholder="hi! your services will be needed at our company X. How quickly can you hop on this?"
+                                    ></textarea>
                                 </div>
                                 <div className="form-control mt-6">
                                     <button className="btn rounded-3xl border-none   bg-[#212531] text-white" onClick={handleSuccess}>
+                                        {" "}
                                         Submit
+                                        <svg width="72" height="22" viewBox="0 0 72 22" xmlns="http://www.w3.org/2000/svg" className="bow-arrow">
+                                            <path fill="none" stroke="#FFFFFF" strokeWidth="2" strokeMiterlimit="0" d="M.043 11.119h70.714M60.917 1.319l9.8 9.8-9.8 9.8"></path>
+                                        </svg>
                                     </button>
                                 </div>
                             </form>
