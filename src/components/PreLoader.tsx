e78874e0fb0a685173b3d32d5b420779f80fbb78
Daniel Olatinsu <danieloluwafolajimi@gmail.com>
2024-09-16 20:14:58 +0100

    Refactor PreLoader component to animate text reveal

--- src/components/PreLoader.tsx
+++ src/components/PreLoader.tsx
@@ -1,7 +1,23 @@
-import React from "react";
+import React, { use, useEffect } from "react";
+import gsap from "gsap";
+import SplitType from "split-type";
 
 const PreLoader = () => {
-    return <div className="min-h-screen flex justify-center items-center w-full text-4xl font-extrabold text-white">PreLoader</div>;
+
+    useEffect(() => {
+        const splitTypes = document.querySelectorAll(".reveal-text");
+
+        splitTypes.forEach((splitType) => {
+            const split = new SplitType(splitType as HTMLElement);
+            gsap.from(split.chars, {
+                duration: 0.01,
+                opacity: 0,
+                y: 10,
+                stagger: 0.02,
+            });
+        });
+    })
+    return <div className="reveal-text min-h-screen flex justify-center items-center w-full text-4xl font-extrabold text-white">PreLoader</div>;
 };
 
 export default PreLoader;
