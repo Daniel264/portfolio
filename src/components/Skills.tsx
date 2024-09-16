c42fa9cdf3d38272a7674939cbaa3a47567e4921
Daniel Olatinsu <danieloluwafolajimi@gmail.com>
2024-09-16 17:54:41 +0100

    add a progress bar for skills

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -5,8 +5,10 @@ const Skills = () => {
         <main className="flex h-full w-full flex-col items-center text-white">
             <h1 className="text-center text-6xl font-bold text-white lg:text-7xl">Skills</h1>
             <p>DEVELOPMENT ARSENAL</p>
-            <section className="grid grid-rows-3 grid-cols-3 gap-4">
-                <article className="row-start-1 col-start-1">
+            <section className="grid grid-cols-3 grid-rows-3 space-x-10">
+                {" "}
+                {/* Increased gap from 4 to 8 */}
+                <article className="col-start-1 row-start-1">
                     <h2 className="text-4xl font-semibold">Frontend</h2>
                     <ul>
                         <li>TypeScript</li>
@@ -14,9 +16,10 @@ const Skills = () => {
                         <li>ViteTest</li>
                         <li>React</li>
                         <li>Next.js</li>
+                        <progress className="progress w-56 progress-primary bg-white" value="95" max="100"></progress>
                     </ul>
                 </article>
-                <article className="row-start-2 col-start-2">
+                <article className="col-start-2 row-start-2">
                     <h2 className="text-4xl font-semibold">Backend</h2>
                     <ul>
                         <li>Node.js</li>
@@ -25,9 +28,10 @@ const Skills = () => {
                         <li>MongoDB</li>
                         <li>PostGreSQL</li>
                         <li>prisma</li>
+                        <progress className="progress w-56 progress-primary bg-white" value="80" max="100"></progress>
                     </ul>
                 </article>
-                <article className="row-start-3 col-start-3">
+                <article className="col-start-3 row-start-3">
                     <h2 className="text-4xl font-semibold">UI Libraries</h2>
                     <ul>
                         <li>GSAP</li>
@@ -36,6 +40,7 @@ const Skills = () => {
                         <li>SCSS</li>
                         <li>CSS</li>
                         <li>ChakraUi</li>
+                        <progress className="progress w-56 progress-primary bg-white" value="100" max="100"></progress>
                     </ul>
                 </article>
             </section>
