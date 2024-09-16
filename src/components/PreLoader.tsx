9a5a325ac4c60de460c1786580a83a8b8be757b1
Daniel Olatinsu <danieloluwafolajimi@gmail.com>
2024-09-16 20:32:26 +0100

    Refactor PreLoader component to animate text reveal and align preloader

--- src/components/PreLoader.tsx
+++ src/components/PreLoader.tsx
@@ -3,21 +3,31 @@ import gsap from "gsap";
 import SplitType from "split-type";
 
 const PreLoader = () => {
-
     useEffect(() => {
-        const splitTypes = document.querySelectorAll(".reveal-text");
+        const splitTypes = document.querySelectorAll(".preloader");
 
         splitTypes.forEach((splitType) => {
             const split = new SplitType(splitType as HTMLElement);
-            gsap.from(split.chars, {
-                duration: 0.01,
-                opacity: 0,
-                y: 10,
-                stagger: 0.02,
-            });
+            gsap.fromTo(
+                split.chars,
+                {
+                    clipPath: "polygon(0 100%, 100% 100%, 100% 0, 0 0)",
+                    duration: 2,
+                    opacity: 0,
+                    y: 0,
+                    stagger: 0.2,
+                },
+                {
+                    clipPath: "polygon(0 0, 100% 0, 100% 100%, 0 100%)",
+                    duration: 2,
+                    opacity: 1,
+                    y: 0,
+                    stagger: 0.2,
+                }
+            );
         });
-    })
-    return <div className="reveal-text min-h-screen flex justify-center items-center w-full text-4xl font-extrabold text-white">PreLoader</div>;
+    });
+    return <div className="preloader flex min-h-screen w-full items-center justify-center text-4xl font-extrabold text-white">PreLoader</div>;
 };
 
 export default PreLoader;
