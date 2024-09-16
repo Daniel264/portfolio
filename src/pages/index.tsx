c547d0645f7fd2a6897204bfd9a56da52ce8a787
Daniel Olatinsu <danieloluwafolajimi@gmail.com>
2024-09-17 00:13:32 +0100

    reduce time for preloader

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -26,7 +26,7 @@ export default function Home() {
         // Set a timer for 5 seconds to show the preloader
         const timer = setTimeout(() => {
             setLoading(false);
-        }, 5000);
+        }, 2300);
 
         return () => clearTimeout(timer); // Cleanup the timer on component unmount
     }, []);
