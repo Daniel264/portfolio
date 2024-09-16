a7895a449ee627dd85d48b079d49cf5c07aaf222
Daniel Olatinsu <danieloluwafolajimi@gmail.com>
2024-09-16 02:37:50 +0100

    change the link icon in the project section

--- src/pages/portfolio/index.tsx
+++ src/pages/portfolio/index.tsx
@@ -120,7 +120,7 @@ const Portfolio: React.FC = () => {
                             </figure>
                             <div className="card-body relative">
                                 <h2 className="card-title text-3xl text-white">{work.title}</h2>
-                                <p className="text-[#909298] reveal-text">{work.description}</p>
+                                <p className="reveal-text text-[#909298]">{work.description}</p>
                                 <p className="text-xl capitalize text-white md:text-2xl">
                                     {work.languages.map((language) => (
                                         <span key={language} className="mx-2 md:mx-4 ">
@@ -132,8 +132,10 @@ const Portfolio: React.FC = () => {
                                     <div className="rounded-full bg-white px-3 py-1 lg:px-4 lg:py-2">
                                         <i className="fa-brands fa-github mt-2 text-3xl text-black md:text-4xl"></i>
                                     </div>
-                                    <div className="rounded-full bg-white px-1 py-2 pl-3 lg:px-3  lg:py-4 ">
-                                        <i className="fa-solid fa-link text-3xl text-black"></i>
+                                    <div className="rounded-full bg-white px-2 py-2  lg:px-3  lg:py-4 flex h-fit justify-center items-center">
+                                        <svg xmlns="http://www.w3.org/2000/svg" className="h-8 md:w-10" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2">
+                                            <path strokeLinecap="round" strokeLinejoin="round" d="M10 6H6a2 2 0 00-2 2v10a2 2 0 002 2h10a2 2 0 002-2v-4M14 4h6m0 0v6m0-6L10 14"></path>
+                                        </svg>
                                     </div>
 
                                     {/* <button className="btn border-none bg-[#212531] text-white">
