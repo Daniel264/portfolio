d26b17aa0716c3ac29532d51d8614b90399488f2
Daniel Olatinsu <danieloluwafolajimi@gmail.com>
2024-09-17 15:17:18 +0100

    Finish contact me form

--- src/pages/contact/index.tsx
+++ src/pages/contact/index.tsx
@@ -1,10 +1,8 @@
-import TopNavigation from "@/components/TopNavigation";
 import React, { useState, useEffect } from "react";
 import { toast } from "react-toastify";
 
 const Contact = () => {
     const [loading, setLoading] = useState(true);
-
     const [formData, setFormData] = useState({
         name: "",
         email: "",
@@ -13,7 +11,7 @@ const Contact = () => {
     const [submitting, setSubmitting] = useState(false);
     const [error, setError] = useState<string | null>(null);
 
-    const handleChange = (e: { target: { name: any; value: any } }) => {
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
         const { name, value } = e.target;
         setFormData((prevData) => ({
             ...prevData,
@@ -21,8 +19,9 @@ const Contact = () => {
         }));
     };
 
-    const handleSubmit = async (e: { preventDefault: () => void }) => {
+    const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault();
+        setSubmitting(true);
         try {
             const response = await fetch("https://formspree.io/f/xyzgaavq", {
                 method: "POST",
@@ -32,22 +31,24 @@ const Contact = () => {
                 body: JSON.stringify(formData),
             });
             if (response.ok) {
-                setSubmitting(true);
+                handleSuccess();
             } else {
                 setError("Something went wrong. Please try again later.");
             }
         } catch (error) {
             setError("Something went wrong. Please try again later.");
+        } finally {
+            setSubmitting(false);
         }
     };
 
     const handleSuccess = () => {
-        toast.success("successful!");
+        toast.success("Message sent successfully!");
+        setFormData({ name: "", email: "", message: "" });
     };
 
     useEffect(() => {
         const timer = setTimeout(() => {
-            // Simulate a slow network
             setLoading(false);
         }, 1000);
         return () => clearTimeout(timer);
@@ -55,17 +56,19 @@ const Contact = () => {
 
     return (
         <>
-            {submitting ? (
-                <p className="">Submitted</p>
+            {loading ? (
+                <p>Loading...</p>
             ) : (
-                <div id="contact" className="contact-page px-2 py-20 text-center md:px-10 lg:py-0 ">
+                <div id="contact" className="contact-page px-2 py-20 text-center md:px-10 lg:py-0">
                     <div className="hero flex h-full flex-col items-center justify-center pt-20 text-white md:pt-24 lg:flex-row lg:items-start lg:justify-between lg:pb-20 lg:pt-32">
                         <div className="lg:w-1/2 lg:pr-10">
                             <h1 id="#big" className="mb-10 text-5xl font-bold md:text-7xl">
                                 Get In Touch
                             </h1>
                             <p className="reveal-text clash-font text-center text-lg">
-                                Currently, i&apos;m looking for new opportunities. <br /> If you have one, my inbox is always open. <br /> Whether you have a job proposal or question <br />
+                                Currently, I&apos;m looking for new opportunities. <br />
+                                If you have one, my inbox is always open. <br />
+                                Whether you have a job proposal or question <br />
                                 or you just want to say hi, I&apos;ll definitely get back to you.
                             </p>
                         </div>
@@ -75,26 +78,26 @@ const Contact = () => {
                                     <div className="space-y-2 lg:flex lg:space-x-5 lg:space-y-0">
                                         <div className="form-control w-full">
                                             <label className="label text-[#D1D5DB]">Name</label>
-                                            <input type="text" className="input border-2 border-[#212531] bg-transparent  placeholder:text-[#7B7E86]" placeholder="Enter your name..." value={formData.name} onChange={handleChange} required />
+                                            <input type="text" name="name" className="input border-2 border-[#212531] bg-transparent placeholder:text-[#7B7E86]" placeholder="Enter your name..." value={formData.name} onChange={handleChange} required />
                                         </div>
                                         <div className="form-control w-full">
                                             <label className="label text-[#D1D5DB]">Email</label>
-                                            <input type="email" className="input border-2 border-[#212531] bg-transparent  placeholder:text-[#7B7E86]" placeholder="enter your email address..." value={formData.email} onChange={handleChange} required />
+                                            <input type="email" name="email" className="input border-2 border-[#212531] bg-transparent placeholder:text-[#7B7E86]" placeholder="Enter your email address..." value={formData.email} onChange={handleChange} required />
                                         </div>
                                     </div>
                                     <div>
                                         <label className="label text-[#D1D5DB]">Message</label>
                                         <textarea
-                                            className="textarea textarea-md w-full max-w-full border-2 border-[#212531] bg-black  bg-transparent placeholder:text-base placeholder:text-[#7B7E86]"
+                                            name="message"
+                                            className="textarea textarea-md w-full max-w-full border-2 border-[#212531] bg-black bg-transparent placeholder:text-base placeholder:text-[#7B7E86]"
+                                            placeholder="Hi! Your services will be needed at our company X. How quickly can you hop on this?"
                                             value={formData.message}
                                             onChange={handleChange}
-                                            placeholder="hi! your services will be needed at our company X. How quickly can you hop on this?"
                                             required
                                         ></textarea>
                                     </div>
                                     <div className="form-control mt-6">
-                                        <button className="btn rounded-3xl border-none   bg-[#212531] text-white" onClick={handleSuccess}>
-                                            {" "}
+                                        <button type="submit" className="btn rounded-3xl border-none bg-[#212531] text-white">
                                             Submit
                                             <svg width="72" height="22" viewBox="0 0 72 22" xmlns="http://www.w3.org/2000/svg" className="bow-arrow">
                                                 <path fill="none" stroke="#FFFFFF" strokeWidth="2" strokeMiterlimit="0" d="M.043 11.119h70.714M60.917 1.319l9.8 9.8-9.8 9.8"></path>
