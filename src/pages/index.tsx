09c98902f70b89ada58d073071ee53938a769ee3
Daniel Olatinsu <danieloluwafolajimi@gmail.com>
2024-09-17 14:01:10 +0100

    let preloader be in sync with main content

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -17,11 +17,13 @@ import Footer from "@/components/Footer";
 import Skills from "@/components/Skills";
 import PreLoader from "@/components/PreLoader";
 import Loader from "@/components/Loader";
+import { time } from "console";
 
 const Typed = dynamic(() => import("@/components/Typed"), { ssr: false });
 
 export default function Home() {
     const [loading, setLoading] = useState(true);
+    const [blinderFinished, setBlinderFinished] = useState(false);
 
     useEffect(() => {
         // Set a timer for 5 seconds to show the preloader
@@ -35,6 +37,28 @@ export default function Home() {
     useEffect(() => {
         if (!loading && typeof window !== "undefined") {
             gsap.registerPlugin(ScrollTrigger);
+
+            const timeline = gsap.timeline({
+                onComplete: () => {
+                    setBlinderFinished(true);
+                },
+            });
+            timeline.to(".blinder", {
+                y: "-100%",
+                stagger: 0.2,
+                ease: "power4.inOut",
+                duration: 1.5,
+            });
+
+            timeline.to(
+                ".main-content",
+                {
+                    opacity: 1,
+                    duration: 1.5,
+                    ease: "power2.inOut",
+                },
+                "-=1.5"
+            );
 
             const splitTypes = document.querySelectorAll(".reveal-text");
 
@@ -111,68 +135,68 @@ export default function Home() {
             <SEO title="Home" />
             {loading ? (
                 <>
-                    <Loader />
-
                     <PreLoader />
                 </>
             ) : (
                 <>
+                    {!blinderFinished && <Loader />}
                     <TopNavigation />
-
-                    <main className="flex h-full min-h-screen flex-col items-center p-5 pt-28">
-                        <div className="max-w-8xl flex w-full text-white md:m-auto">
-                            <div className="fixed hidden h-full md:left-10 md:top-44 md:flex lg:left-12">
-                                <Menu />
-                            </div>
-                            <div className="ml-0 flex h-full w-full flex-col items-center md:ml-20">
-                                <div className="flex flex-col sm:flex-row">
-                                    <div className="hero bg-inherit">
-                                        <div className="hero-content flex-col-reverse lg:flex-row-reverse">
-                                            <div className="w-full">
-                                                <p className="text-lg text-[rgba(174,174,174,1)]">Hello, my name is </p>
-                                                <h3 className="text-[rgba(251, 251, 251,1)] font-scale my-5 text-center text-4xl sm:text-5xl md:text-7xl" style={{ clipPath: "polygon(0 0, 100% 0, 100% 100%, 0% 100%)" }}>
-                                                    <span
-                                                        id="big"
-                                                        className="font-monasans text-[3.4rem] font-extrabold leading-tight text-[#E4DED7] sm:text-8xl md:tracking-wider
+                    <div>
+                        <main className="flex h-full min-h-screen flex-col items-center p-5 pt-28">
+                            <div className="max-w-8xl flex w-full text-white md:m-auto">
+                                <div className="fixed hidden h-full md:left-10 md:top-44 md:flex lg:left-12">
+                                    <Menu />
+                                </div>
+                                <div className="ml-0 flex h-full w-full flex-col items-center md:ml-20">
+                                    <div className="flex flex-col sm:flex-row">
+                                        <div className="hero bg-inherit">
+                                            <div className="hero-content flex-col-reverse lg:flex-row-reverse">
+                                                <div className="w-full">
+                                                    <p className="text-lg text-[rgba(174,174,174,1)]">Hello, my name is </p>
+                                                    <h3 className="text-[rgba(251, 251, 251,1)] font-scale my-5 text-center text-4xl sm:text-5xl md:text-7xl" style={{ clipPath: "polygon(0 0, 100% 0, 100% 100%, 0% 100%)" }}>
+                                                        <span
+                                                            id="big"
+                                                            className="font-monasans text-[3.4rem] font-extrabold leading-tight text-[#E4DED7] sm:text-8xl md:tracking-wider
                                                     lg:text-9xl"
-                                                    >
-                                                        DANIEL <br />
-                                                        OLATINSU
-                                                    </span>
-                                                </h3>
-                                                <p className="pt-10 text-2xl font-light">
-                                                    <span className="font-semibold">I&apos;m a</span> <Typed />
-                                                </p>
+                                                        >
+                                                            DANIEL <br />
+                                                            OLATINSU
+                                                        </span>
+                                                    </h3>
+                                                    <p className="pt-10 text-2xl font-light">
+                                                        <span className="font-semibold">I&apos;m a</span> <Typed />
+                                                    </p>
+                                                </div>
                                             </div>
                                         </div>
                                     </div>
-                                </div>
 
-                                <div className="pb-8 md:hidden">
-                                    <Menu />
-                                </div>
-                                <div className="ml-0 mt-20 space-y-4 md:ml-0 md:mr-44 md:mt-16 md:space-x-5">
-                                    <button className="btn w-full rounded border-none bg-[#212531] text-white shadow-2xl hover:bg-slate-500 md:w-44" onClick={() => (window.location.href = "mailto:danieloluwafolajimi@gmail.com")}>
-                                        Lets Talk <i className="fa-solid fa-message"></i>
-                                    </button>
-                                    <button onClick={handleLoading} className="btn w-full rounded border-2 border-[#212531] bg-transparent text-white shadow-2xl hover:bg-slate-500 md:w-44">
-                                        Download CV <i className="fa-solid fa-download"></i>
-                                    </button>
+                                    <div className="pb-8 md:hidden">
+                                        <Menu />
+                                    </div>
+                                    <div className="ml-0 mt-20 space-y-4 md:ml-0 md:mr-44 md:mt-16 md:space-x-5">
+                                        <button className="btn w-full rounded border-none bg-[#212531] text-white shadow-2xl hover:bg-slate-500 md:w-44" onClick={() => (window.location.href = "mailto:danieloluwafolajimi@gmail.com")}>
+                                            Lets Talk <i className="fa-solid fa-message"></i>
+                                        </button>
+                                        <button onClick={handleLoading} className="btn w-full rounded border-2 border-[#212531] bg-transparent text-white shadow-2xl hover:bg-slate-500 md:w-44">
+                                            Download CV <i className="fa-solid fa-download"></i>
+                                        </button>
+                                    </div>
                                 </div>
                             </div>
-                        </div>
-                    </main>
+                        </main>
 
-                    <hr className="mx-auto w-[80%] pb-10" />
-                    <About />
-                    <hr className="mx-auto w-[80%]" />
-                    <Portfolio />
-                    <hr className="mx-auto w-[80%] pb-10" />
-                    <Skills />
-                    <hr className="mx-auto w-[80%]" />
-                    <Contact />
-                    <hr className="mx-auto w-[80%]" />
-                    <Footer />
+                        <hr className="mx-auto w-[80%] pb-10" />
+                        <About />
+                        <hr className="mx-auto w-[80%]" />
+                        <Portfolio />
+                        <hr className="mx-auto w-[80%] pb-10" />
+                        <Skills />
+                        <hr className="mx-auto w-[80%]" />
+                        <Contact />
+                        <hr className="mx-auto w-[80%]" />
+                        <Footer />
+                    </div>
                 </>
             )}
         </div>
