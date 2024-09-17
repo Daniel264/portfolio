c741351483e0dfa1b45eaeccde75b53273dc985e
Daniel Olatinsu <danieloluwafolajimi@gmail.com>
2024-09-17 13:28:17 +0100

    Refactor loader component styles

--- src/components/Loader.tsx
+++ src/components/Loader.tsx
@@ -2,13 +2,13 @@ import React from "react";
 
 const Loader = () => {
     return (
-        <div className="loader">
-            <div className="blinder-container">
-                <div className="blinder"></div>
-                <div className="blinder"></div>
-                <div className="blinder"></div>
-                <div className="blinder"></div>
-                <div className="blinder"></div>
+        <div className="loader absolute z-[999] h-[100vh] w-full">
+            <div className="blinder-container absolute top-0 flex h-[100vh] w-full">
+                <div className="blinder h-[100vh] flex-1 origin-top bg-[#8da8a2]"></div>
+                <div className="blinder h-[100vh] flex-1 origin-top bg-[#8da8a2]"></div>
+                <div className="blinder h-[100vh] flex-1 origin-top bg-[#8da8a2]"></div>
+                <div className="blinder h-[100vh] flex-1 origin-top bg-[#8da8a2]"></div>
+                <div className="blinder h-[100vh] flex-1 origin-top bg-[#8da8a2]"></div>
             </div>
         </div>
     );
