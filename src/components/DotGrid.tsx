eec650ab04c119dac757c48c9dc1fefd18d9ab2a
Daniel… <danieloluwafolajimi@gmail.com>
2024-09-26 21:25:46 +0100

    Refactor DotGrid component to remove unused code and commented out sections

--- src/components/DotGrid.tsx
+++ src/components/DotGrid.tsx
@@ -1,17 +1,27 @@
-
 const WaterDropGrid = () => {
     return (
-        <div>
+        <div className="relative grid h-screen place-content-center bg-slate-900">
             <DotGrid />
         </div>
-    )
+    );
 };
 
 const GRID_WIDTH = 25;
 const GRID_HEIGHT = 25;
 
 const DotGrid = () => {
-    return <div></div>
-}
+    const dots = [];
+    let index = 0;
+
+    for (let i = 0; i < GRID_WIDTH; i++) {
+        for (let j = 0; j < GRID_HEIGHT; j++) {
+            // dots.push(
+            //     <Dot key={index} x={i} y={j} />
+            // );
+            // index++;
+        }
+    }
+    return <div></div>;
+};
 
 export default WaterDropGrid;
