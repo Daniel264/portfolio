f241b92dc32cf243828e62913247bead31f458bf
Daniel… <danieloluwafolajimi@gmail.com>
2024-09-26 17:02:14 +0100

    add variants for animation

--- src/components/Reveal.tsx
+++ src/components/Reveal.tsx
@@ -7,7 +7,12 @@ interface Props {
 export const Reveal = ({ children }: Props) => {
     return (
         <div className="reveal">
-            <motion.div>{children}</motion.div>
+            <motion.div
+            variants={{
+                hidden: { opacity: 0, y: 70 },
+                visible: { opacity: 1, y: 0 },
+            }}
+            >{children}</motion.div>
         </div>
     );
 };
