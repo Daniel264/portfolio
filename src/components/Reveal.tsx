049a7aa56fa83bcb1a5773f8aea3d63b27c0089e
Daniel… <danieloluwafolajimi@gmail.com>
2024-09-26 17:12:19 +0100

    finish the reveal animation

--- src/components/Reveal.tsx
+++ src/components/Reveal.tsx
@@ -7,24 +7,28 @@ interface Props {
 
 export const Reveal = ({ children }: Props) => {
     const ref = useRef(null);
-    const isInView = useInView(ref, {once: true});
+    const isInView = useInView(ref, { once: true });
+    const mainControls = useAnimation();
 
     useEffect(() => {
-        console.log(isInView);
-        
+        if (isInView) {
+            mainControls.start("visible");
+        }
     }, [isInView]);
 
     return (
         <div ref={ref} className="reveal">
             <motion.div
-            variants={{
-                hidden: { opacity: 0, y: 70 },
-                visible: { opacity: 1, y: 0 },
-            }}
-            initial="hidden"
-            animate="visible"
-            transition={{ duration: 0.5, delay: 0.25 }}
-            >{children}</motion.div>
+                variants={{
+                    hidden: { opacity: 0, y: 70 },
+                    visible: { opacity: 1, y: 0 },
+                }}
+                initial="hidden"
+                animate={mainControls}
+                transition={{ duration: 0.5, delay: 0.25 }}
+            >
+                {children}
+            </motion.div>
         </div>
     );
 };
