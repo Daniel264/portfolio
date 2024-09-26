457737e2500dc2943599e59fe8dbf8f2742dbb2e
Daniel… <danieloluwafolajimi@gmail.com>
2024-09-26 17:05:31 +0100

    use useRef to select the main div

--- src/components/Reveal.tsx
+++ src/components/Reveal.tsx
@@ -1,17 +1,22 @@
 import { motion, useInView, useAnimation } from "framer-motion";
+import { useRef } from "react";
 
 interface Props {
     children: JSX.Element;
 }
 
 export const Reveal = ({ children }: Props) => {
+    const ref = useRef(null);
     return (
-        <div className="reveal">
+        <div ref={ref} className="reveal">
             <motion.div
             variants={{
                 hidden: { opacity: 0, y: 70 },
                 visible: { opacity: 1, y: 0 },
             }}
+            initial="hidden"
+            animate="visible"
+            transition={{ duration: 0.5, delay: 0.25 }}
             >{children}</motion.div>
         </div>
     );
