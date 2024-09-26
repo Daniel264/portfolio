5b573bfc987e18073e7ce8691c5b81178e4d447d
Daniel… <danieloluwafolajimi@gmail.com>
2024-09-26 21:35:42 +0100

    finish making the dots

--- src/components/DotGrid.tsx
+++ src/components/DotGrid.tsx
@@ -15,13 +15,15 @@ const DotGrid = () => {
 
     for (let i = 0; i < GRID_WIDTH; i++) {
         for (let j = 0; j < GRID_HEIGHT; j++) {
-            // dots.push(
-            //     <Dot key={index} x={i} y={j} />
-            // );
-            // index++;
+            dots.push(
+                <div className="cursor-crosshair rounded-full p-2 transition-colors hover:bg-slate-600" data-index={index} key={`${i}-${j}`}>
+                    <div className="dot-point h-2 w-2 rounded-full bg-gradient-to-b from-slate-700 to-slate-400 opacity-50" data-index={index} />
+                </div>
+            );
+            index++;
         }
     }
-    return <div></div>;
+    return <div style={{ gridTemplateColumns: `repeat(${GRID_WIDTH}, 1fr)` }}>{dots}</div>;
 };
 
 export default WaterDropGrid;
