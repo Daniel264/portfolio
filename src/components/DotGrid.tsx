694a9ba65bdeabb686abf5d7ceb1323f7c3c0059
Daniel… <danieloluwafolajimi@gmail.com>
2024-09-26 21:23:17 +0100

    Add WaterDropGrid component to DotGrid.tsx

---
+++ src/components/DotGrid.tsx
@@ -0,0 +1,17 @@
+
+const WaterDropGrid = () => {
+    return (
+        <div>
+            <DotGrid />
+        </div>
+    )
+};
+
+const GRID_WIDTH = 25;
+const GRID_HEIGHT = 25;
+
+const DotGrid = () => {
+    return <div></div>
+}
+
+export default WaterDropGrid;
