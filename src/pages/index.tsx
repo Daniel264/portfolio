27e2c6ad696734f0c793b9edd0f680ec8c123508
Daniel… <danieloluwafolajimi@gmail.com>
2024-09-30 10:54:56 +0100

    Refactor font usage in Home component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -181,7 +181,7 @@ export default function Home() {
                                                         <h3 className="text-[rgba(251, 251, 251,1)] font-scale my-5 text-center text-4xl sm:text-5xl md:text-7xl" style={{ clipPath: "polygon(0 0, 100% 0, 100% 100%, 0% 100%)" }}>
                                                             <span
                                                                 id="big"
-                                                                className="formula_condensed text-[3.4rem] font-extrabold leading-tight text-[#E4DED7] sm:text-9xl md:tracking-wider
+                                                                className="formula_condensed text-[3.4rem] font-extrabold leading-tight  text-[#E4DED7] sm:text-9xl md:tracking-wider
                                                         lg:text-9xl"
                                                             >
                                                                 DANIEL <br />
@@ -260,7 +260,7 @@ export default function Home() {
                                                                     setIsHovered(false);
                                                                 }}
                                                                 id="big"
-                                                                className="formula_condensed text-[3.4rem] font-extrabold leading-tight text-black sm:text-8xl md:tracking-wider
+                                                                className="formula_condensed text-[3.4rem] font-extrabold leading-tight text-[#000] sm:text-9xl
                                                         lg:text-9xl"
                                                             >
                                                                 ダニエル <br />
