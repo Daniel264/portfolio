31b1b8722605618cf95b83b593a038a506d6f9b8
Daniel… <danieloluwafolajimi@gmail.com>
2024-09-30 23:39:09 +0100

    let an empty div show onclick

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,40 +1,28 @@
-import React from "react";
+// import React from "react";
 
-const Header = () => {
-    return (
-        <div>
-            <div className="navbar bg-base-100">
-                <div className="flex-1">
-                    <a className="btn btn-ghost text-xl">DO</a>
-                </div>
-                <div className="flex-none">
-                    <div className="dropdown dropdown-end">
-                        <div tabIndex={0} role="button" className=" btn btn-ghost">
-                            <div className="w-10 rounded-full">
-                                <svg className="h-5 w-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
-                                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 1h15M1 7h15M1 13h15" />
-                                </svg>
-                            </div>
-                        </div>
-                        <ul tabIndex={0} className="menu dropdown-content menu-sm z-[1] mt-3 w-52 rounded-box bg-base-100 p-2 shadow">
-                            <li>
-                                <a className="justify-between">
-                                    Profile
-                                    <span className="badge">New</span>
-                                </a>
-                            </li>
-                            <li>
-                                <a>Settings</a>
-                            </li>
-                            <li>
-                                <a>Logout</a>
-                            </li>
-                        </ul>
-                    </div>
-                </div>
-            </div>
-        </div>
-    );
-};
+// const menu = {
+//     open: {
+//         width: "480px",
+//         height: "650px",
+//         top: "-25px",
+//         right: "-25px",
+//         transition: {duration: 0.75, type: "tween", ease: [0.76, 0, 0.24, 1]}
+//     },
+//     closed: {
+//         width: "100px",
+//         height: "40px",
+//         top: "0px",
+//         right: "0px",
+//         transition: {duration: 0.75, type: "tween", ease: [0.76, 0, 0.24, 1]}
+//     }
+// }
 
-export default Header;
+// const Header = () => {
+//     const [is]
+//     return 
+//     (
+
+//     );
+// };
+
+// export default Header;
--- src/components/SIdeMenu.tsx
+++ src/components/SIdeMenu.tsx
@@ -2,14 +2,32 @@
 
 import React, { useState } from "react";
 import Button from "./Button";
+import { motion } from "framer-motion";
+
+const menu = {
+    open: {
+        width: "480px",
+        height: "650px",
+        top: "-25px",
+        right: "-25px",
+        transition: { duration: 0.75, type: "tween", ease: [0.76, 0, 0.24, 1] },
+    },
+    closed: {
+        width: "100px",
+        height: "40px",
+        top: "0px",
+        right: "0px",
+        transition: { duration: 0.75, type: "tween", ease: [0.76, 0, 0.24, 1] },
+    },
+};
 
 const SideMenu = () => {
     const [isActive, setIsActive] = useState(false);
     return (
-        <div>
-            <div className="fixed right-[50px] top-[50px]">
+        <div className="fixed right-[50px] top-[50px]">
+            <motion.div className="w-[480px] h-[650px] bg-[#c9fd74] rounded-3xl relative" variants={menu} animate={isActive ? "open" : "closed"} initial="open">
                 <Button isActive={isActive} toggleMenu={() => setIsActive(!isActive)} />
-            </div>
+            </motion.div>
         </div>
     );
 };
