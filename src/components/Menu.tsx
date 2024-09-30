a4fc663d095ba3422d68959e1ef0fb871177e4b2
Daniel… <danieloluwafolajimi@gmail.com>
2024-09-30 13:28:37 +0100

    adjust the size of menus

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -24,22 +24,22 @@ const Menu = () => {
         <div>
             <ul className=" menu flex flex-row rounded-box bg-inherit md:flex md:flex-col">
                 <li className="my-menu">
-                    <a href="https://twitter.com/DanielOlatinsu" className="tooltip tooltip-right transition-transform hover:scale-125" data-tip="Twitter" target="_blank" rel="noopener noreferrer ">
+                    <a href="https://twitter.com/DanielOlatinsu" className="tooltip tooltip-right scale-75 transition-transform hover:scale-125" data-tip="Twitter" target="_blank" rel="noopener noreferrer ">
                         <i className="fab fa-twitter fa-2x"></i> {/* Use fa-2x to scale the icon */}
                     </a>
                 </li>
                 <li className="my-menu">
-                    <a href="https://www.instagram.com/_.daniel._fo/" className="tooltip tooltip-right transition-transform hover:scale-125" data-tip="Instagram" target="_blank" rel="noopener noreferrer">
+                    <a href="https://www.instagram.com/_.daniel._fo/" className="tooltip tooltip-right scale-75 transition-transform hover:scale-125" data-tip="Instagram" target="_blank" rel="noopener noreferrer">
                         <i className="fab fa-instagram fa-2x"></i> {/* Use fa-2x to scale the icon */}
                     </a>
                 </li>
                 <li className="my-menu">
-                    <a href="https://www.linkedin.com/in/daniel-olatinsu-09415a230/" className="tooltip tooltip-right transition-transform hover:scale-125" data-tip="LinkedIn" target="_blank" rel="noopener noreferrer">
+                    <a href="https://www.linkedin.com/in/daniel-olatinsu-09415a230/" className="tooltip tooltip-right transition-transform scale-75 hover:scale-125" data-tip="LinkedIn" target="_blank" rel="noopener noreferrer">
                         <i className="fab fa-linkedin fa-2x"></i> {/* Use fa-2x to scale the icon */}
                     </a>
                 </li>
                 <li className="my-menu">
-                    <a href="https://github.com/Daniel264" className="tooltip tooltip-right transition-transform hover:scale-125" data-tip="GitHub" target="_blank" rel="noopener noreferrer">
+                    <a href="https://github.com/Daniel264" className="tooltip tooltip-right transition-transform scale-75 hover:scale-125" data-tip="GitHub" target="_blank" rel="noopener noreferrer">
                         <i className="fab fa-github fa-2x"></i> {/* Use fa-2x to scale the icon */}
                     </a>
                 </li>
