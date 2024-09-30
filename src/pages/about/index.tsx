1446632bda36da764c35e01f7892ce89c1aa2540
Daniel… <danieloluwafolajimi@gmail.com>
2024-09-30 23:53:55 +0100

    make the about page aligned well for responsiveness

--- src/pages/about/index.tsx
+++ src/pages/about/index.tsx
@@ -86,9 +86,9 @@ const About = ({ background, text, onMouseEnter, onMouseLeave, paragraph }: Prop
                 <div className="loading-animation"></div>
             ) : (
                 <>
-                    <main className="flex min-h-screen w-full flex-col items-center justify-center px-[2rem] pt-24 text-center md:pt-0 lg:px-[8rem]">
+                    <main className="flex min-h-screen w-full flex-col items-center justify-center px-[2rem] pt-32 lg:pt-24 text-center md:pt-0 lg:px-[8rem]">
                         <div className="flex h-10 items-center rounded px-3 pb-10 w-full">
-                            <h1 onMouseEnter={onMouseEnter} onMouseLeave={onMouseLeave} className=" font-semibold text-7xl formula_condensed text-left uppercase pb-14 sm:pb-0 sm:ml-24">
+                            <h1 onMouseEnter={onMouseEnter} onMouseLeave={onMouseLeave} className=" font-semibold text-7xl formula_condensed text-left uppercase pb-14  sm:pb-0 sm:ml-24">
                                 About The Creator
                             </h1>
                         </div>
