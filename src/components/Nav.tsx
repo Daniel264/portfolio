0222fda8be58c243d55a5a5c689d31facf29a9a4
Daniel… <danieloluwafolajimi@gmail.com>
2024-10-01 15:52:05 +0100

    map the footer link to the sidebar

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { motion } from "framer-motion";
 import { links } from "@/utilities/link";
 import { perspective } from "@/Animations/Nav";
+import { footerLinks } from "@/utilities/link";
 
 const Nav = () => {
     return (
@@ -13,6 +14,18 @@ const Nav = () => {
                         <div key={`b_${i}`} className="linkContainer">
                             <motion.div href={href} custom={i} variants={perspective} initial="initial" animate="enter" exit="exit">
                                 <a className="text-5xl leading-relaxed text-black">{title}</a>
+                            </motion.div>
+                        </div>
+                    );
+                })}
+            </div>
+            <div className="flex flex-row gap-4">
+                {footerLinks.map((link, i) => {
+                    const { title, href } = link;
+                    return (
+                        <div key={`b_${i}`}>
+                            <motion.div>
+                                <a className="text-lg">{title}</a>
                             </motion.div>
                         </div>
                     );
