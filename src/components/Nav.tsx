f6032c0927a35cd850be0a719edf5c45ed3bbc3e
Daniel… <danieloluwafolajimi@gmail.com>
2024-10-01 16:35:10 +0100

    Refactor Nav component to add gsap animation to nav links

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,10 +1,28 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { motion } from "framer-motion";
 import { links } from "@/utilities/link";
 import { perspective } from "@/Animations/Nav";
 import { footerLinks } from "@/utilities/link";
+import gsap from "gsap";
 
 const Nav = () => {
+    useEffect(() => {
+        gsap.to(".lin", {
+            opacity: 1,
+            y: -50,
+            stagger: 0.3,
+            duration: 1.4,
+            rotateX: 30,
+        });
+    }, []);
+    // gsap.to(".links", {
+    //     opacity: 1,
+    //     y: 50,
+    //     delay: 2,
+    //     stagger: 0.3,
+
+    // });
+
     return (
         <div className="box-border flex h-full flex-col justify-between px-[40px] pb-[50px] pt-[100px]">
             <div className="flex flex-col gap-[10px] ">
@@ -24,9 +42,9 @@ const Nav = () => {
                     const { title, href } = link;
                     return (
                         <div key={`b_${i}`}>
-                            <motion.div>
-                                <a className="text-lg">{title}</a>
-                            </motion.div>
+                            <div className="lin opacity-0">
+                                <a className="links text-lg">{title}</a>
+                            </div>
                         </div>
                     );
                 })}
