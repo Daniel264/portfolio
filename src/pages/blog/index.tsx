41938c9579ea3df7ecc3684946830823cd3bda9c
Daniel… <danieloluwafolajimi@gmail.com>
2024-10-01 16:49:36 +0100

    Add initial structure for the blog page

---
+++ src/pages/blog/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Blog = () => {
+    return (
+        <div className="flex h-screen w-full items-center justify-center">
+            <p className="text-4xl">Coming soon ...</p>
+        </div>
+    );
+};
+
+export default Blog;
