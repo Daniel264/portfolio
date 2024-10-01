7c31cbd1f22f4c2d02b90610c398c9709e63d186
Daniel… <danieloluwafolajimi@gmail.com>
2024-10-01 15:10:16 +0100

    Create Nav component for displaying navigation links

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -4,14 +4,14 @@ import { links } from "@/utilities/link";
 
 const Nav = () => {
     return (
-        <div>
-            <div>
+        <div className="box-border flex h-full flex-col justify-between px-[40px] pb-[50px] pt-[100px]">
+            <div className="flex flex-col gap-[10px] ">
                 {links.map((link, i) => {
                     const { title, href } = link;
                     return (
-                        <div key={`b_${i}`} className="">
+                        <div key={`b_${i}`} className="linkContainer">
                             <motion.div href={href} custom={i} variants={perspective} initial="initial" animate="enter" exit="exit">
-                                <a>{title}</a>
+                                <a className="text-4xl text-black">{title}</a>
                             </motion.div>
                         </div>
                     );
